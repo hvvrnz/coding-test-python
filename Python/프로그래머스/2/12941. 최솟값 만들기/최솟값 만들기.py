cd273b483e8dee32e696efc6899a180b9cd2268f
hwangryoonjeong <163698023+hvvrnz@users.noreply.github.com>
2026-09-03 10:38:20 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.32 ms, Memory: 11.2 MB -BaekjoonHub

--- Python/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ Python/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,15 +1,11 @@
 def solution(A,B):
-    total_1 = 0
-    total_2 = 0
+    total = 0
     
     a = sorted(A)
     rb = sorted(B, reverse=True)
-    ra = sorted(A, reverse=True)
-    b = sorted(B)
     
     for i in range(len(A)):
-        total_1 += a[i] * rb[i]
-        total_2 += ra[i] * b[i]
+        total += a[i] * rb[i]
     
-    return total_1 if total_1 < total_2 else total_2
+    return total
         
