df4e02b2f9807ab967ce486441c609e0dadb9dc5
hwangryoonjeong <163698023+hvvrnz@users.noreply.github.com>
2026-09-03 10:01:11 +0900

    [level 2] Title: 올바른 괄호, Time: 4.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,11 @@
+def solution(s):
+    stack = []
+    for ch in s:
+        if ch == "(":
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
