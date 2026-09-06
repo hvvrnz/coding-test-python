c58bcb2b0d8350ccba869779759bafd9b774f2d1
hwangryoonjeong <163698023+hvvrnz@users.noreply.github.com>
2026-09-06 12:11:54 +0900

    [level 2] Title: 귤 고르기, Time: 13.33 ms, Memory: 23.1 MB -BaekjoonHub

--- Python/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ Python/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,10 @@
+import collections
+
 def solution(k, tangerine):
-    remain = k
-    d = {}
-    
-    for size in tangerine:
-        if size in d:
-            d[size] += 1
-        else:
-            d[size] = 1
-            
-    tangerine_list = [total for size, total in sorted(d.items(), key=lambda x: x[1], reverse=True)]
-    
-    for i in range(len(tangerine_list)):
-        remain -= tangerine_list[i]
-        if remain <= 0:
-            return i + 1
+    result = 0
+    cnt = collections.Counter(tangerine)
+    for i in sorted(cnt.values(), reverse=True):
+        k -= i
+        result += 1
+        if k <= 0:
+            return result
